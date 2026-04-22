4925408496bb12e407b549f99454493cc93c9352
Nhlamulo <nhlamokhoza57@gmail.com>
2026-04-22 15:31:30 +0200

    routes

--- backend/src/routes/clients.js
+++ backend/src/routes/clients.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const clientsController = require('../controllers')
+const clientsController = require('../controllers/clientsController');
+
+router.post('/', clientsController.createClient);
+router.get('/', clientsController.getAllClients);
+router.put('/:id', clientsController.updateClient);
+router.delete('/:id', clientsController.deleteClient);
+
+module.exports = router;
