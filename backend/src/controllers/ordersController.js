a53f1279c52f661c6743043318cc6b66f555e21e
Nhlamulo <nhlamokhoza57@gmail.com>
2026-04-22 12:13:14 +0200

    orders control is done

--- backend/src/controllers/ordersController.js
+++ backend/src/controllers/ordersController.js
@@ -77,4 +77,47 @@ const getOrderById =async (req, res) => {
     }
 };
 
-const updateOrder = async (req, res) => {};
+const updateOrder = async (req, res) => {
+    const client = await pool.connect();
+    
+    try {
+        await client.query('BEGIN');
+        const {garment_type, status, deadline, price, deposit, fabric_notes, notes, measurements} = req.body;
+        const {rows} = await client.query(
+            `UPDATE orders SET garment_type=$1, status=$2, deadlne=$3, price=$4,
+            deposit=$5, fabric_notes=$6, notes=$7 WHERE id=$8  RETURNING *`,
+            [garment_type, status, deadline, price, deposit, fabric_notes, notes, req.params.id]
+        );
+        if (!rows.length) return res.status(404).json({error: 'Order not found'});
+        if (measurements) {
+            await client.query(
+                `INSERT INTO measurements (order_id, bust, waist, hips, shoulder_width, length, sleeve_length)
+                VALUES ($1, $2, $3, $4, $5, $6, $7)
+                ON CONFLICT (order_id) DO UPDATE
+                SET bust=$2, waist=$3, hips=$4, shoulder_width=$5, length=$6, sleeve_length=$7`,
+                [req.params.id, bust, waist, hips, shoulder_width, length, sleeve_length]
+            );
+        }
+        await client.query('COMMIT');
+        res.json(rows[0]);
+    }
+    catch (err) {
+        await client.query('ROLLBACK');
+        res.status(500).json({error: 'Failed to update order'});
+    }
+    finally { client.release(); }
+};
+
+const deleteOrder = async (req, res) => {
+    try{
+        const {rowCount} = await pool.query(
+            'DELETE FROM orders WHERE id = $1',
+            [req.params.id]
+        );
+        if (!rowCount) return res.status(404).json({error: 'Ornder not found'});
+        res.status(204).send();
+    }
+    catch {}
+};
+
+module.exports = {createOrder, getAllOrders, getOrderById,updateOrder, deleteOrder};
